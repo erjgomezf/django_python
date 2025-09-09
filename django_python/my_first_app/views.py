d5a4d10c38618b33cae93a7f95b9ac1c7724f1b0
Ernesto Gomez <erjgomezf@gmail.com>
2025-09-09 10:28:47 -0400

    Agregar vista y plantilla para mostrar la lista de autos, y registrar la aplicación en la configuración.

--- django_python/my_first_app/views.py
+++ django_python/my_first_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def my_view(request):
+    car_list = [
+        {"title": "Toyota"},
+        {"title": "Honda"},
+        {"title": "Ford"},
+    ]
+    context = {
+        "car_list": car_list
+    }
+    return render(request, "my_first_app/car_list.html", context)
